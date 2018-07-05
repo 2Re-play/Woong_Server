3d6e9dd0228ed635a4facff25a82c8a5acaae864
김진영 <gam0za@naver.com>
2018-07-05 21:29:58 +0900

    cm

--- Woong_Server/controller/index.js
+++ Woong_Server/controller/index.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+
+const router = express.Router()
 
 /* GET home page. */
-router.get('/hi', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.send('hi');
-});
+router.get('/hi', (req, res) => {
 
-module.exports = router;
+  // res.render('index', { title: 'Express' });
+  res.send('hi')
+})
+module.exports = router
