009da189aa379a023d162ac2d9ed197bbca13403
LeeDongsu <30032dongsu@moducampus.com>
2018-07-07 04:47:54 +0900

    initialize routes/index.js

--- Woong_Server/src/routes/index.js
+++ Woong_Server/src/routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express')
-const food_type = require('./routes/food_type')
-const account = require('./routes/account')
-
+const category = require('./category')
+const cart = require('./cart')
+const food_type = require('./food_type')
+const weeklyfarmer = require('./weeklyFarmer') // 이 주의 농부 
+const review = require('./review') // 리뷰
+const bookmark = require('./bookmark') // 즐겨찾기
 
 const router = express.Router()
 
 /* GET home page. */
+router.use('/category', category)
+router.use('/cart', cart)
 router.use('/food_type', food_type)
-router.use('/account', account)
-console.log('index')
+router.use('/weeklyfarmer', weeklyfarmer) 
+router.use('/review', review)
+router.use('/bookmark', bookmark)
+
 
 module.exports = router
