5ec3dda7f58b58e582a59c68b2ed742b80d7d5dc
LeeDongsu <30032dongsu@moducampus.com>
2018-07-07 04:18:26 +0900

    dev/dongsu

--- Woong_Server/src/controller/categoryController.js
+++ Woong_Server/src/controller/categoryController.js
@@ -1,9 +1,29 @@
-const selectTypeFromFood = (req, res) => {
-  console.log('Access Select food type')
+const Joi = require('joi')
+const categoryModel = require('models/categoryModel')
+
+const dbConnection = require('../../lib/dbConnection')
+
+const getSubCategoryListController = async (req, res) => {
+  const { main_id } = req.params
+  
+  // const data = _.cloneDeep(body)
+  const validation = Joi.validate(main_id, Joi.number().required())
+  
+  if (validation.error) {
+    return
+  }
+
+  const connection = await dbConnection()
+  console.log(connection)
+  try {
+    const category_info = await categoryModel.selectSubListByMain(connection, main_id)
+  } catch (e) {
+
+  }
   res.send('ok')
 }
 
 
 module.exports = {
-  selectTypeFromFood,
+  getSubCategoryListController,
 }
--- Woong_Server/src/routes/category.js
+++ Woong_Server/src/routes/category.js
@@ -4,6 +4,6 @@ const categoryCtrl = require('controller/categoryController')
 const category = express.Router()
 
 /* GET home page. */
-category.get('/main/:main_id', categoryCtrl.selectTypeFromFood)
+category.get('/main/:main_id', categoryCtrl.getSubCategoryListController)
 
 module.exports = category
