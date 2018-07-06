bf5f5ceffc216c07a26964b084f3ab0bee375ca7
LeeDongsu <30032dongsu@moducampus.com>
2018-07-07 06:21:41 +0900

    add Subcategory list api

--- Woong_Server/src/controller/categoryController.js
+++ Woong_Server/src/controller/categoryController.js
@@ -6,21 +6,25 @@ const dbConnection = require('lib/dbConnection')
 const getSubCategoryListController = async (req, res) => {
   const { main_id } = req.params
   
-  // const data = _.cloneDeep(body)
   const validation = Joi.validate(main_id, Joi.number().required())
   
   if (validation.error) {
-    return
+    throw new Error(validation.error)
   }
 
   const connection = await dbConnection()
-  console.log(connection)
   try {
     const category_info = await categoryModel.selectSubListByMain(connection, main_id)
+    const data = {
+      category_info,
+    }
+    res.status(200)
+    res.send({ data })
   } catch (e) {
-
+    res.status(500)
+    res.send(e)
   }
-  res.send('ok')
+  connection.release()
 }
 
 
