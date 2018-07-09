7a569003953bf53cfcdc59f94b594508dec8e619
LeeDongsu <30032dongsu@moducampus.com>
2018-07-10 04:42:13 +0900

    [수정] Update Query selectItemByKeyWord

--- Woong_Server/src/models/itemModel.js
+++ Woong_Server/src/models/itemModel.js
@@ -3,6 +3,7 @@ exports.selectItemByKeyword = (connection, { user_id }, keyword) => {
     const Query = `
     SELECT
       i.item_id, i.market_id, i.main_id, i.sub_id,
+      i.item_name,
       m.market_name, i.item_unit, i.item_price, m.quick, m.delivery,
       S.user_id, ii.file_key,
       IF(isnull(S.user_id), 0, IF(S.user_id = 1, 1, 0)) as favorite_flag
