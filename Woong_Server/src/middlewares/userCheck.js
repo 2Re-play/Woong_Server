f3a6f6bf7638e11073d181d7ee338eab98127fea
dla1635 <dla1635@gmail.com>
2018-07-11 20:11:16 +0900

    test

--- Woong_Server/src/middlewares/userCheck.js
+++ Woong_Server/src/middlewares/userCheck.js
@@ -8,13 +8,8 @@ module.exports = async (req, res, next) => {
   const { usertoken } = req.headers
   // console.log(usertoken)
   try {
-<<<<<<< HEAD
-    req.user = await token.decode(usertoken, 'JvaWQiLCJkZXZpY2VfdG9rZW4isInNldF90a')
-    // console.log(req.user)
-=======
     req.user = await token.decode(usertoken, secretKey)
     // console.log())
->>>>>>> b5ae90d34ea2f39fe33a887925de0c4e068ec652
     if (_.isEmpty(req.user)) {
       throw new Error('user Authentication Error')
     } 
