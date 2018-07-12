de8cd95418248c341074e167b6d563e7f887c36d
LeeDongsu <30032dongsu@moducampus.com>
2018-07-12 19:29:06 +0900

    마켓별 상품리스트 수정

--- Woong_Server/src/models/categoryModel.js
+++ Woong_Server/src/models/categoryModel.js
@@ -19,7 +19,7 @@ exports.selectItemByMarket = (connection, { main_id, sub_id }, { user_id }) => {
   return new Promise((resolve, reject) => {
     const Query = `
     SELECT
-      i.item_id, i.market_id, i.main_id, i.sub_id,
+      i.item_id, i.market_id, i.main_id, i.sub_id, i.item_name
       m.market_name, i.item_unit, i.item_price, m.quick, m.delivery,
       S.user_id, ii.file_key,
       IF(isnull(S.user_id), 0, IF(S.user_id = ${user_id}, 1, 0)) as favorite_flag
