115140a7888d763bf767619f3d72f261f473b896
jaehyun <wogusduddnjs@gmail.com>
2018-07-14 00:00:25 +0900

    search history

--- Woong_Server/src/controller/historyController.js
+++ Woong_Server/src/controller/historyController.js
@@ -5,44 +5,30 @@ const { respondJson, respondOnError } = require('../lib/response')
 
 
 const post_location_history = async (req, res) => {
-
-  const connection = await dbConnection()  
   const { user_id } = req.user
   const { history_name } = req.body
-  
-
-  console.log(req.body)
-  console.log(user_id)
-  console.log(history_name)
-
-
-  const sheme = {
-    user_id: Joi.number().required(),
-    history_name: Joi.string().required(),
-  }
-
-  const validation_data = {
+  const { longitude } = req.body
+  const { latitude } = req.body
+  let data = {}
+  data = {
     user_id,
     history_name,
+    latitude,
+    longitude,
   }
-  
+  console.log(data)
+  const connection = await dbConnection() 
   try {
-
-    const input_validation = Joi.validate(validation_data, sheme)
-
-    if (input_validation.error) {
-      throw new Error(403)
+    console.log(2)
+    const result = await historyModel.post_location_history(connection, data)
+    console.log(result)
+    const info = {
+      result,
     }
-
-    const post_location_history_result = await historyModel.post_location_history(connection, history_name, user_id)
-    console.log(history_name)
-    const data = {
-      post_location_history_result,
-    }
-    respondJson('성공적인 위치 히스토리 저장!!', data, res, 200)
+    respondJson('성공적인 위치 히스토리 저장!!', info, res, 200)
 
   } catch (e) {
-
+    console.log(e)
     if (e.message === '403') { 
       respondOnError('형식이 맞지 않습니다.', res, 403) 
     } else {
@@ -72,8 +58,6 @@ const get_location_history = async (req, res) => {
 
     const get_location_history_result = await historyModel.get_location_history(connection, user_id)
    
-
-    console.log(get_location_history_result)
     const data = {
       get_location_history_result,
     }
@@ -82,10 +66,7 @@ const get_location_history = async (req, res) => {
 
     if (e.message === '403') {
       respondOnError('형식이 맞지 않습니다.', res, 403)
-    } else {
-      respondOnError('서버 내부 에러', res, 500)
-    }
-
+    } 
   } finally {
     connection.release()
   }
